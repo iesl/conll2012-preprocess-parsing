ba81d63b3b7185e864b72d37f0476d66e8b0b27e
Emma Strubell <emma.strubell@gmail.com>
2017-12-20 14:21:26 -0500

    clean up

--- bin/convert-bilou.sh
+++ bin/convert-bilou.sh
@@ -6,8 +6,6 @@
 
 input_file=$1
 
-m=`awk '{print NF}' $input_file | sort -n | tail -1`; echo $m; start=15; l=`seq $start $m`;
-
 max_field=`awk '{print NF}' $input_file | sort -n | tail -1`
 first_field=14
 fields_to_convert=`seq $first_field $(( max_field - 1 ))`
@@ -22,11 +20,6 @@ for field in $fields_to_convert; do
     echo "Converting field $field of $(( max_field - 1 ))"
     bin/convert-bilou-single-field.sh $bilou_file $field > $tmpfile
     cp $tmpfile $bilou_file
-    #paste <(awk '{if (substr($1,1,1) !~ /#/ ) {print $1"\t"$2"\t"$3"\t"$4"\t"$5}}' $f) \
-    #    <(awk '{print $2}' $f_pos) \
-    #    <(awk '{print $6"\t"$7"\t"$9}' $f_converted) \
-    #    <(awk '{if (substr($1,1,1) !~ /#/ ) {print $0}}' $f | tr -s ' ' | cut -d' ' -f7- | sed 's/ /\t/g') \
-    #> "$input_file.bilou"
 done
 
 rm $tmpfile

ef7d893bd2208c1219415a7f940645ad26fcd0aa
Emma Strubell <emma.strubell@gmail.com>
2017-12-20 14:19:38 -0500

    convert bilou

--- bin/convert-bilou.sh
+++ bin/convert-bilou.sh
@@ -1,43 +1,33 @@
 #!/bin/bash
 #
-# Converts the given field (arg2) in the given file(arg1) to BILOU format 
+# Converts the fields defined below in the given file (arg1) to BILOU format 
 # from the CoNLL-2012 segment format.
 #
 
 input_file=$1
-field=$2
 
-awk -v f="$field" \
-'BEGIN{curr=""; inside=0} {
-  if (!NF){
-    print $0
-  }
-  else if (NF-f-1 < 0){
-    print "-"
-  }
-  else {
-    t=$f; 
-    if (t == "*"){
-      if(inside){
-        t="I-"curr
-      } else{ 
-        t="O"
-      }
-    }
-    if (t ~ "\\(.*" && t ~ ".*\\)"){
-      type=gensub(/\(([A-Za-z0-9-]*).*\)/, "\\1", 1, t)
-      t="U-"type
-    }
-    else if (t ~ "\\(.*"){
-      type=gensub(/\((.*)\*/, "\\1", 1, t)
-      t="B-"type
-      curr=type
-      inside=1
-    }
-    else if (t ~ ".*\\)"){
-      t=gensub(/(.*)\)/, "L-"curr, 1, t)
-      inside=0
-    }
-    print $f"\t"t
-  }
-}' $1
+m=`awk '{print NF}' $input_file | sort -n | tail -1`; echo $m; start=15; l=`seq $start $m`;
+
+max_field=`awk '{print NF}' $input_file | sort -n | tail -1`
+first_field=14
+fields_to_convert=`seq $first_field $(( max_field - 1 ))`
+
+tmpfile=`mktemp`
+
+bilou_file="$input_file.bilou"
+cp $input_file $bilou_file
+
+# Finally, paste the original file together with the dependency parses and auto pos tags
+for field in $fields_to_convert; do
+    echo "Converting field $field of $(( max_field - 1 ))"
+    bin/convert-bilou-single-field.sh $bilou_file $field > $tmpfile
+    cp $tmpfile $bilou_file
+    #paste <(awk '{if (substr($1,1,1) !~ /#/ ) {print $1"\t"$2"\t"$3"\t"$4"\t"$5}}' $f) \
+    #    <(awk '{print $2}' $f_pos) \
+    #    <(awk '{print $6"\t"$7"\t"$9}' $f_converted) \
+    #    <(awk '{if (substr($1,1,1) !~ /#/ ) {print $0}}' $f | tr -s ' ' | cut -d' ' -f7- | sed 's/ /\t/g') \
+    #> "$input_file.bilou"
+done
+
+rm $tmpfile
+

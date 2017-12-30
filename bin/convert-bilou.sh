3250d86f31aca7c77c2f8451d0973e7f44ba6ffd
Emma Strubell <emma.strubell@gmail.com>
2017-12-29 21:22:04 -0500

    remove old file

--- bin/convert-bilou.sh
+++ bin/convert-bilou.sh
@@ -19,7 +19,7 @@ cp $input_file $bilou_file
 
 for field in $fields_to_convert; do
     echo "Converting field $field of $(( max_field - 1 ))"
-    bin/convert-bilou-single-field.py --input_file $bilou_file --field $((field - 1)) --bilou > $tmpfile
+    python bin/convert-bilou-single-field.py --input_file $bilou_file --field $((field - 1)) --bilou $bilou > $tmpfile
     cp $tmpfile $bilou_file
 done
 

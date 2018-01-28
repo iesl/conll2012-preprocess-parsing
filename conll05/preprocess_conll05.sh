307c6442c536d9bf4c43e54d0f43c43fad85c473
Emma Strubell <emma.strubell@gmail.com>
2018-01-28 13:15:06 -0500

    preprocess conll first try

--- conll05/preprocess_conll05.sh
+++ conll05/preprocess_conll05.sh
@@ -13,7 +13,7 @@ pos_config=$CLEARNLP/config_decode_pos.xml
 # by the converter
 echo "Extracting trees from: $input_file"
 # word pos parse -> stick words, pos into parse as terminals
-zcat $input_file | sed 's/(/-RRB-/g' | sed 's/(/-LRB-/g' |awk '{print $1" "$2"\t"$3}' | sed 's/\(.*\)\t\(.*\)\*\(.*\)/\2(\1)\3/' > "$input_file.parse"
+zcat $input_file | awk '{gsub(/\(/, "-RRB-", $1); gsub(/\)/, "-LRB-", $1); gsub(/\(/, "-RRB-", $2); gsub(/\)/, "-LRB-", $2); print $1" "$2"\t"$3}' | sed 's/\(.*\)\t\(.*\)\*\(.*\)/\2(\1)\3/' > "$input_file.parse"
 
 
 # Now convert those parses to dependencies

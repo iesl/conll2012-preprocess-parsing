92c66c2c459efb3f50591cbe6272c21d81a3e210
Emma Strubell <emma.strubell@gmail.com>
2018-01-29 14:07:05 -0500

    read gzips

--- bin/extract_conll_prop_file.py
+++ bin/extract_conll_prop_file.py
@@ -7,6 +7,7 @@ arg_parser.add_argument('--input_file', type=str, help='File to process')
 arg_parser.add_argument('--word_field', type=int, help='Field containing words')
 arg_parser.add_argument('--pred_field', type=int, help='Field containing predicates')
 arg_parser.add_argument('--pred_field_offset', type=int, help='Offset for predicates field', default=1)
+arg_parser.add_argument('--take_last', type=bool, help='Whether to take the last field', default=False)
 
 arg_parser.add_argument('--first_prop_field', type=int, help='First field containing props')
 
@@ -18,7 +19,7 @@ with gzip.open(args.input_file, 'r') if args.input_file.endswith('gz') else open
     line = line.strip()
     if line:
       split_line = line.strip().split()
-      props = split_line[args.first_prop_field:-1]
+      props = split_line[args.first_prop_field:] if args.take_last else split_line[args.first_prop_field:-1]
       word = split_line[args.word_field] if split_line[args.pred_field + args.pred_field_offset] != '-' else '-'
       new_fields = [word] + props
       new_line = '\t'.join(new_fields)

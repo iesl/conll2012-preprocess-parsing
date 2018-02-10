5d91474f3e0c33015480f371ead1fef66fd68f32
Emma Strubell <emma.strubell@gmail.com>
2018-02-10 13:24:02 -0500

    add sent ids

---
+++ bin/add_sent_ids.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+import os
+import argparse
+
+arg_parser = argparse.ArgumentParser(description='Add sent ids to CoNLL-2012 file')
+arg_parser.add_argument('--input_file', type=str, help='File to process')
+
+args = arg_parser.parse_args()
+
+if not os.path.exists(args.output_dir):
+    os.makedirs(args.output_dir)
+
+with open(args.input_file, 'r') as in_f:
+    current_doc = ''
+    sent_num = 0
+    for line_num, line in enumerate(in_f):
+        line = line.strip()
+        # blank line means end of sentence
+        if not line:
+            sent_num += 1
+        else:
+            split_line = line.split('\t')
+            this_doc = split_line[0]
+            if this_doc != current_doc:
+                currect_doc = this_doc
+                sent_num = 0
+            split_line[1] = sent_num
+            print('\t'.join(split_line))
+

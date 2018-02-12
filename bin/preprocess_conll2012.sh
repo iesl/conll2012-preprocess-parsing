245446124e1348f085db031dc39938fead15d089
Emma Strubell <emma.strubell@gmail.com>
2018-02-12 17:54:22 -0500

    fix

--- bin/preprocess_conll2012.sh
+++ bin/preprocess_conll2012.sh
@@ -16,7 +16,7 @@ for f in `find $input_dir -type f -not -path '*/\.*' -name "*_conll"`; do
     # word pos parse -> stick words, pos into parse as terminals
     awk '{if (substr($1,1,1) !~ /#/ ) print $5" "$4"\t"$6}' $f | \
     sed 's/\(.*\)\t\(.*\)\*\(.*\)/\2(\1)\3/' | \
-    awk '{if(!NF && substr($1,1,1) !~ /\(/){print "(TOP(INTJ(UH XX)))"} else {print}}' > "$f.parse"
+    awk '{if(NF && substr($1,1,1) !~ /\(/){print "(TOP(INTJ(UH XX)))"} else {print}}' > "$f.parse"
 done
 
 # Now convert those parses to dependencies

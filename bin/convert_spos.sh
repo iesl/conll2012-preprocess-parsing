627edeb1a2c7463d30b5ce3ebb38afe018f46951
Patrick Verga <pat@cs.umass.edu>
2018-02-13 12:46:04 -0500

    Create convert_spos.sh
    add script to convert spos data

---
+++ bin/convert_spos.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+##
+## convert spos.conllu  data to 15 column tsv format
+##
+
+
+if [ "$#" -ne 1 ]; then
+  echo "Must supply input directory containing containing wsj\*sdep.spos.conllu files."
+  exit 1
+fi
+
+in_dir=$1
+out_dir=${in_dir}/"bio_format"
+mkdir -p $out_dir
+
+for in_f in wsj02-21-trn.sdep.spos.conllu wsj22-dev.sdep.spos.conllu wsj23-tst.sdep.spos.conllu;
+do
+  awk -F $'\t' '{\
+    printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", \
+    "DOCID", "DOC_NUM", ($1-1), $2, $5, $4, $7, $8, "DUMMY", "DUMMY", "DUMMY", "DUMMY", "DUMMY", "NER", "COREF"\
+  }' $in_dir/$in_f > ${out_dir}/${in_f}_BIO
+done

5feefa2f0d074f296b4a87a9fd7de1b77f711cd9
Emma Strubell <emma.strubell@gmail.com>
2018-02-14 16:51:29 -0500

    fix

--- conll05/preprocess_conll05_sdeps.sh
+++ conll05/preprocess_conll05_sdeps.sh
@@ -42,7 +42,8 @@ java -Xmx8g -cp $STANFORD_CP edu.stanford.nlp.tagger.maxent.MaxentTagger \
 f_converted="$input_file.parse.sdeps"
 f_pos="$input_file.parse.sdeps.pos"
 f_combined="$f_converted.combined"
-paste <(zcat $input_file | awk '{if(NF == 0){print ""} else {print "_\t_\t_\t"$1"\t"$2}}' ) \
+paste <(zcat $input_file | awk '{if(NF == 0){print ""} else {print "_\t_\t_\t"$1}}' ) \
+    <(awk '{print $5}' $f_converted) \
     <(awk '{print $2}' $f_pos) \
     <(awk '{if(NF == 0){print ""} else {print $7"\t"$8"\t_"}}' $f_converted) \
     <(zcat $input_file | awk '{if(NF == 0){print ""} else {print $5"\t"$6"\t-\t-\t"$4}}' ) \

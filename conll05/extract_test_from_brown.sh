56c02e779f26f676931bed51e0fc85109066eda6
Emma Strubell <emma.strubell@gmail.com>
2018-02-14 16:28:26 -0500

    fix

--- conll05/extract_test_from_brown.sh
+++ conll05/extract_test_from_brown.sh
@@ -9,5 +9,5 @@ mkdir -p $CONLL05/test.brown/synt
 rm $CONLL05/test.brown/synt/test.brown.synt.gz
 
 for section in $SECTIONS; do
-    cat $BROWN/CK/CK* | awk '{if($1 !~ "*x*") print}' | $SRLCONLL/bin/wsj-removetraces.pl | $SRLCONLL/bin/wsj-to-se.pl -w 0 -p 1 | gzip >> $CONLL05/test.brown/synt/test.brown.synt.gz
+    cat $BROWN/CK/CK$section | awk '{if($1 !~ "*x*") print}' | $SRLCONLL/bin/wsj-removetraces.pl | $SRLCONLL/bin/wsj-to-se.pl -w 0 -p 1 | gzip >> $CONLL05/test.brown/synt/test.brown.synt.gz
 done

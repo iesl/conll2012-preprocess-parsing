a849c72a8a12364b7c2246429f12ace9a9e24f6f
Emma Strubell <emma.strubell@gmail.com>
2018-02-21 20:39:20 -0500

    fix

--- ptb/convert_spos.sh
+++ ptb/convert_spos.sh
@@ -14,7 +14,7 @@ mkdir -p $out_dir
 for in_f in wsj02-21-trn.sdep.spos.conllu wsj22-dev.sdep.spos.conllu wsj23-tst.sdep.spos.conllu;
 do
   awk '{if(NF){ printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", \
-  "-", "-", ($1-1), $2, $5, $4, $7, $8, "-", "-", "-", "-", "-", "*", "-"} \
+  "-", "-", ($1-1), $2, $4, $5, $7, $8, "-", "-", "-", "-", "-", "*", "-"} \
   else {print} \
   }' $in_dir/$in_f > ${out_dir}/${in_f}_BIO
 done

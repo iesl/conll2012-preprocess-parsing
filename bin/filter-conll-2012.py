22cd18c295ffddfc3f29d3f042b9c49c95281933
Emma Strubell <emma.strubell@gmail.com>
2018-07-03 11:43:27 -0400

    filter

--- bin/filter-conll-2012.py
+++ bin/filter-conll-2012.py
@@ -6,14 +6,16 @@ from glob import glob
 
 arg_parser = argparse.ArgumentParser(description='Filter CoNLL-12 files by docid and concatenate')
 arg_parser.add_argument('--input_dir', type=str, help='Directory to process')
-arg_parser.add_argument('--docid_file', type=str, help='List of doc ids to keep')
+arg_parser.add_argument('--docid_file', type=str, default='', help='List of doc ids to keep')
 args = arg_parser.parse_args()
 
 docid_map = set()
-with open(args.docid_file, 'r') as f:
-  for line in f:
-    line = line.strip()
-    docid_map.add(line)
+
+if args.docid_file != '':
+  with open(args.docid_file, 'r') as f:
+    for line in f:
+      line = line.strip()
+      docid_map.add(line)
 
 fnames = [d for f in os.walk(args.input_dir) for d in glob(os.path.join(f[0], '*.combined'))]
 

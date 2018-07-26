9c00b8aa6d56e1a540144770c375d936c89896ca
Emma Strubell <emma.strubell@gmail.com>
2018-07-26 18:10:10 -0400

    convert script for conll05

---
+++ conll05/convert-bio.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Converts the fields defined below in the given file (arg1) to BILOU format 
+# from the CoNLL-2012 segment format.
+#
+
+input_file=$1
+
+bilou_arg="--bio"
+
+max_field=`awk '{print NF}' $input_file | sort -n | tail -1`
+first_field=14
+fields_to_convert=`seq $first_field $(( max_field ))`
+
+tmpfile=`mktemp`
+
+bilou_file="$input_file.bio"
+cp $input_file $bilou_file
+
+for field in $fields_to_convert; do
+    echo "Converting field $field of $(( max_field ))"
+    echo "bin/convert-bilou-single-field.py --input_file $bilou_file --field $((field - 1)) $bilou_arg"
+    python bin/convert-bilou-single-field.py --input_file $bilou_file --field $((field - 1)) $bilou_arg > $tmpfile
+    cp $tmpfile $bilou_file
+done
+
+rm $tmpfile
+

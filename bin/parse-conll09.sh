bc573ee92ce7a5d681d13965634d8bd809e34217
Emma Strubell <emma.strubell@gmail.com>
2018-08-17 15:16:02 -0400

    09

--- bin/parse-conll09.sh
+++ bin/parse-conll09.sh
@@ -4,6 +4,8 @@
 # - Download the Stanford parser: https://nlp.stanford.edu/software/lex-parser.shtml
 # - Make sure you set the STANFORD_PARSER environment variable, e.g:
 #     export STANFORD_PARSER="$HOME/canvas/stanford-parser-full-2017-06-09"
+# - This script expects that you have already created $output_dir, and will fail otherwise
+
 
 STANFORD_CP="$STANFORD_PARSER/*:"
 
@@ -33,7 +35,7 @@ java -Xmx8g -cp $STANFORD_CP edu.stanford.nlp.trees.EnglishGrammaticalStructure 
 # Finally, paste the original file together with the dependency parses and auto pos tags
 f_parsed="$output_dir/$input_file_nopath.deps"
 f_combined="$output_dir/$input_file_nopath.parsed"
-paste <(awk 'BEGIN{s=0} {if (substr($1,1,1) !~ /#/ && NF != 0) {print $1"\t"$2"\t"$3"\t"$4"\t"$5} else {print ""; s++}}' $input_file) \
+paste <(awk 'BEGIN{s=0} {if NF != 0) {print $1"\t"$2"\t"$3"\t"$4"\t"$5} else {print ""; s++}}' $input_file) \
       <(awk '{if(NF == 0){print ""} else {print $5"\t"$6"\t"$7"\t"$8"}}' $f_parsed) \
-      <(awk '{if (substr($1,1,1) !~ /#/ ) {print $0}}' $input_file | tr -s ' ' | cut -d' ' -f8- | sed 's/ /\t/g') \
+      <(awk '{if(NF == 0){print ""}}' $input_file | tr -s ' ' | cut -d' ' -f8- | sed 's/ /\t/g') \
 > $f_combined

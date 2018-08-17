dafc9d1d7a9852d05893f137ee0b432830cd0c39
Emma Strubell <emma.strubell@gmail.com>
2018-08-17 14:26:58 -0400

    09

--- bin/parse-conll09.sh
+++ bin/parse-conll09.sh
@@ -14,7 +14,7 @@ output_dir=$2
 input_file_nopath=${input_file##*/}
 
 # Convert to one-sentence-per-line format and parse
-awk '{print $4}' $input_file | awk '{if($1 == ""){print ""} else {printf "%s ", $0}} END {print ""}' | \
+awk '{print $2}' $input_file | awk '{if($1 == ""){print ""} else {printf "%s ", $0}} END {print ""}' | \
 java -Xmx8g -cp $STANFORD_CP edu.stanford.nlp.parser.lexparser.LexicalizedParser \
 -sentences newline \
 -outputFormat penn \
@@ -33,7 +33,7 @@ java -Xmx8g -cp $STANFORD_CP edu.stanford.nlp.trees.EnglishGrammaticalStructure 
 # Finally, paste the original file together with the dependency parses and auto pos tags
 f_parsed="$output_dir/$input_file_nopath.deps"
 f_combined="$output_dir/$input_file_nopath.parsed"
-paste <(awk 'BEGIN{s=0} {if (substr($1,1,1) !~ /#/ && NF != 0) {print $1"\t"s"\t"$3"\t"$4"\t"$5} else {print ""; s++}}' $input_file) \
-      <(awk '{if(NF == 0){print ""} else {print $5"\t"$7"\t"$8"\t_"}}' $f_parsed) \
-      <(awk '{if (substr($1,1,1) !~ /#/ ) {print $0}}' $input_file | tr -s ' ' | cut -d' ' -f9- | sed 's/ /\t/g') \
+paste <(awk 'BEGIN{s=0} {if (substr($1,1,1) !~ /#/ && NF != 0) {print $1"\t"$2"\t"$3"\t"$4"\t"$5} else {print ""; s++}}' $input_file) \
+      <(awk '{if(NF == 0){print ""} else {print $5"\t"$6"\t"$7"\t"$8"}}' $f_parsed) \
+      <(awk '{if (substr($1,1,1) !~ /#/ ) {print $0}}' $input_file | tr -s ' ' | cut -d' ' -f8- | sed 's/ /\t/g') \
 > $f_combined

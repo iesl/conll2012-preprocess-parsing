092cb40e88fef9a164f171cb0b0377d2c44347ab
Emma Strubell <emma.strubell@gmail.com>
2018-08-22 11:21:08 -0400

    fix

--- bin/parse-conll09.sh
+++ bin/parse-conll09.sh
@@ -16,26 +16,26 @@ output_dir=$2
 input_file_nopath=${input_file##*/}
 
 # Convert to one-sentence-per-line format and parse
-awk '{print $2}' $input_file | awk '{if($1 == ""){print ""} else {printf "%s ", $0}} END {print ""}' | \
-java -Xmx8g -cp $STANFORD_CP edu.stanford.nlp.parser.lexparser.LexicalizedParser \
--sentences newline \
--outputFormat penn \
--tokenized \
--originalDependencies \
-edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz \
-- > $output_dir/$input_file_nopath.trees
-
-# Convert parses to dependencies
-java -Xmx8g -cp $STANFORD_CP edu.stanford.nlp.trees.EnglishGrammaticalStructure \
--$dependencies_option \
--conllx \
--treeFile $output_dir/$input_file_nopath.trees \
-> $output_dir/$input_file_nopath.deps
+#awk '{print $2}' $input_file | awk '{if($1 == ""){print ""} else {printf "%s ", $0}} END {print ""}' | \
+#java -Xmx8g -cp $STANFORD_CP edu.stanford.nlp.parser.lexparser.LexicalizedParser \
+#-sentences newline \
+#-outputFormat penn \
+#-tokenized \
+#-originalDependencies \
+#edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz \
+#- > $output_dir/$input_file_nopath.trees
+#
+## Convert parses to dependencies
+#java -Xmx8g -cp $STANFORD_CP edu.stanford.nlp.trees.EnglishGrammaticalStructure \
+#-$dependencies_option \
+#-conllx \
+#-treeFile $output_dir/$input_file_nopath.trees \
+#> $output_dir/$input_file_nopath.deps
 
 # Finally, paste the original file together with the dependency parses and auto pos tags
 f_parsed="$output_dir/$input_file_nopath.deps"
 f_combined="$output_dir/$input_file_nopath.parsed"
 paste <(awk 'BEGIN{s=0} {if NF != 0) {print $1"\t"$2"\t"$3"\t"$4"\t"$5} else {print ""; s++}}' $input_file) \
-      <(awk '{if(NF == 0){print ""} else {print $5"\t"$6"\t"$7"\t"$8"}}' $f_parsed) \
+      <(awk '{if(NF == 0){print ""} else {print $5"\t"$6"\t"$7"\t"$8}}' $f_parsed) \
       <(awk '{if(NF == 0){print ""}}' $input_file | tr -s ' ' | cut -d' ' -f8- | sed 's/ /\t/g') \
 > $f_combined
